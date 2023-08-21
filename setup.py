b9075864c90ea40d9af980a543908aa0b221d665
Max Palehin <ars_kit@icloud.com>
2023-08-21 15:47:53 +0300

    add logging in webhook

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='WalletPay',
-    version='1.2.1',
+    version='1.2.2',
     packages=find_packages(),
     install_requires=[
         'requests',
