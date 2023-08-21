57354bc8db09ecffc869ebdbf726e6e3a221a674
Max Palehin <ars_kit@icloud.com>
2023-08-21 17:55:16 +0300

    bugfixes webhook

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='WalletPay',
-    version='1.2.2',
+    version='1.2.4',
     packages=find_packages(),
     install_requires=[
         'requests',
