31986fb52fc783353ef0032338c11e1a9ee76bef
RemmoDY <remmody@yandex.ru>
2024-03-04 11:11:05 +0500

    remove all files from git staging area

--- build/lib/WalletPay/__init__.py
+++
@@ -1,4 +0,0 @@
-from WalletPay.WalletPayAPI import WalletPayAPI
-from WalletPay.AsyncWalletPayAPI import AsyncWalletPayAPI
-from WalletPay.WebhookManager import WebhookManager
-from WalletPay import types
--- build/lib/WalletPay/types/__init__.py
+++
@@ -1,4 +0,0 @@
-from WalletPay.types.OrderPreview import OrderPreview
-from WalletPay.types.Exception import WalletPayException
-from WalletPay.types.OrderReconciliationItem import OrderReconciliationItem
-from WalletPay.types.WebhookData import Event
